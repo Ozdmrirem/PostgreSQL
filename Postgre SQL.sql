8a8d8cada80baee3b816e6a787e5ebd13c225f75
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-04-29 23:52:21 +0300

    Add files via upload

---
+++ Postgre SQL.sql
@@ -0,0 +1,22 @@
+--create table urun
+--(
+	--Id integer primary key not null,
+	--ad varchar(15) not null,
+	--marka varchar(20),
+	--stok integer,
+	--kategori varchar(15)
+--)
+
+--create table kategori
+--(	
+	--Id integer not null primary key,
+	--ad text
+--)
+
+-- create table deneme
+-- (
+-- 	deneme varchar(20)
+-- )
+
+--drop table kategori
+
---
+++ Postgre SQL1.sql
@@ -0,0 +1,2 @@
+SELECT * FROM public.musteri
+ORDER BY "Id" ASC

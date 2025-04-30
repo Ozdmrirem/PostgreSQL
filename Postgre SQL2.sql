219998034899da46c174f9344d57818718e32934
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-05-01 01:57:42 +0300

    Add files via upload

---
+++ Postgre SQL2.sql
@@ -0,0 +1,12 @@
+--insert into musteri ("Id","ad","soyad","sehir") values (6,'Ayşe','Yıldırım','Ankara')
+--SELECT * FROM public.musteri
+--insert into musteri ("Id","ad","soyad","sehir") values (7,'Mehmet','Kaya','Bursa
+--insert into musteri ("Id","ad","soyad","sehir") values (8,'Tufan','Çimen','İstanbul')
+--insert into musteri ("Id","ad","soyad","sehir") values (9,'Zeynep','Öztürk','Adana')
+--insert into musteri ("Id","ad","soyad","sehir") values (10,'Nalan','Tunç','Adana')
+
+--Select "Id",ad,soyad FROM musteri
+
+--Select * from musteri where ad='Mehmet'
+
+--Select * from musteri where sehir='Ankara' and "Id"=4 or sehir='Adana' or soyad='Kaya'

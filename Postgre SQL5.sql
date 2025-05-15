c37ad3262fc60feaaf0b7ce3e3b96c6015775615
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-05-15 23:30:18 +0300

    Add files via upload

--- Postgre SQL5.sql
+++ Postgre SQL5.sql
@@ -31,11 +31,58 @@
 --Select * from bolum
 --select * from fakulte
 
---Select bolumid,bolumad,ad from fakulte left join bolum on fakulte.id=bolum.bolumf
+--Select bolumid,bolumad,ad from fakulte left join bolum 
+--on fakulte.id=bolum.bolumf
 
---Select id,ad,bolumad from bolum right join fakulte on fakulte.id=bolum.bolumf
+--Select id,ad,bolumad from bolum right join fakulte 
+--on fakulte.id=bolum.bolumf
 
+--Select musteri.ad,soyad,sehir,meslek.ad from musteri full join meslek
+--on musteri.meslek=meslek.id
 
+--Select bolumad,ad from bolum cross join fakulte
 
+ -- Select * from bolum2
+ -- intersect
+ -- Select * from bolum3 
 
- 
+ -- Select * from bolum2
+ -- except
+ -- Select * from bolum3 
+
+--Select * from bolum2 union select * from bolum3 order by id
+
+--Select * from bolum2 union all select * from bolum3 order by id
+
+--Select ASCII ('a') -->97
+--Select ASCII ('A') -->65
+
+--Select CONCAT('Günaydın','SQL','Dersleri') --> GünaydınSQLDersleri
+
+--Select CONCAT_WS('*','Günaydın','SQL','Dersleri') --> Günaydın*SQL*Dersleri
+
+--Select LEFT ('Merhaba Dünya',3) --> Mer
+
+--Select RIGHT ('Merhaba Dünya',3) --> nya
+
+--Select LENGTH('Merhaba Dünya')  --> 13
+
+--Select id,replace(ad,'A','a') from bolum3
+
+--Select reverse(ad) from bolum3
+
+--Select substring('İrem Özdemir',6,2) -->Öz
+
+--Select Lower(ad),upper(ad) from bolum3
+
+--Select abs(-5) -->5
+
+--Select ceiling(4.85) -->5
+
+--Select floor(4.85) -->4
+
+--Select PI() -->3.141592653589793
+
+--Select power(2,4) -->16
+
+--Select Random() 

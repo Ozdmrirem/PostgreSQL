eb3689706163178257a9bf0c11f3a57be9ee317f
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-05-16 23:42:06 +0300

    Add files via upload

---
+++ Postgre SQL6.sql
@@ -0,0 +1,18 @@
+--Select * from kitaplar
+--insert into kitaplar (id,ad,yazar,tarih) values (6,'Satranç','Zweig','01.09.2020')
+
+--Select Current_Date --> 2025-05-16
+
+--Select Current_Time --> "23:26:31.476134+03:00"
+--Select Current_Time(1) -->"23:27:53.900000+03:00"
+
+--Select Now() --> "2025-05-16 23:28:25.309321+03"
+
+--Select age(timestamp '06.07.2000') --> "24 years 10 mons 10 days"
+
+--Select ad,tarih,age(now(),tarih) from kitaplar --> hangi kitap ne zaman kaydoldu
+
+
+
+
+
